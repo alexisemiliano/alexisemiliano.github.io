9e32eeb4ee5efbae0e665d752441802332f55c6a
alexisemiliano <98563817+alexisemiliano@users.noreply.github.com>
2022-01-28 20:02:17 -0500

    Add files via upload

--- js/myScripts.js
+++ js/myScripts.js
@@ -9,6 +9,65 @@ function closeNav() {
   document.getElementById("mySidebar").style.display = "none";
 }
 
+// Map
+var map = L.map('map');
+
+map.createPane('labels');
+
+// This pane is above markers but below popups
+map.getPane('labels').style.zIndex = 650;
+
+// Layers in this pane are non-interactive and do not obscure mouse/touch events
+map.getPane('labels').style.pointerEvents = 'none';
+
+var cartodbAttribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="https://carto.com/attribution">CARTO</a>';
+
+var positron = L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}.png', {
+    attribution: cartodbAttribution
+}).addTo(map);
+
+var positronLabels = L.tileLayer('http://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}.png', {
+    attribution: cartodbAttribution,
+    pane: 'labels'
+}).addTo(map);
+
+map.setView({lat: 19.4326, lng: -99.1332}, 17);
+
+
+
+// Localization
+var x = document.getElementById("positionUser");
+
+function getLocation() {
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
+}
+
+function showPosition(position) {
+  x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
+    map.setView({lat: position.coords.latitude, lng: position.coords.longitude}, 17);
+    var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
+}
+
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
 
 // Modal Image Gallery
 function onClick(element) {
