305a0709e7b1179092efcea66d2f5ca3b18e8d83
alexisemiliano <98563817+alexisemiliano@users.noreply.github.com>
2022-01-30 09:58:46 -0500

    Add files via upload

--- js/myScripts.js
+++ js/myScripts.js
@@ -40,11 +40,12 @@ var x = document.getElementById("positionUser");
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(showPosition,showError);
   } else {
     x.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
+
 
 function showPosition(position) {
   x.innerHTML = "Latitude: " + position.coords.latitude + "<br>Longitude: " + position.coords.longitude;
